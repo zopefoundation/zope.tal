ff4bc56406d36398355ccd6b7b9f50ca02f59b1d
Christian Theune <ct@gocept.com>
2007-04-22 11:11:04 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.tal',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.tal',
       license='ZPL 2.1',
       description='Zope 3 Template Application Languate (TAL)',
