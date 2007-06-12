b29a1100c1b5f7d5107dc38e60dcdf320be84c40
Godefroid Chapelle <gotcha@bubblenet.be>
2007-06-12 09:56:46 +0000

    comments about the workaround

--- src/zope/tal/talpypy/zptest/appzpt.py
+++ src/zope/tal/talpypy/zptest/appzpt.py
@@ -5,3 +5,13 @@ import random
 def appfunc():
     return zope.tal.zptest.test(random.randrange(3))
 
+#
+# the code hereunder doesn't work : 
+# it results in an ImportError
+#
+# from zope.tal import zptest
+# import random
+
+# def appfunc():
+#     return zptest.test(random.randrange(3))
+
