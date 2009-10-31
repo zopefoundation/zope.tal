e5250b233ee0de400e437ee4fa2bd71ebadf20c4
Vincent Fretin <vincent.fretin@gmail.com>
2009-10-31 17:14:20 +0000

    Back to development: 3.5.3dev

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def alltests():
     return TestSuite(suites)
 
 setup(name='zope.tal',
-      version = '3.5.2',
+      version = '3.5.3dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope 3 Template Application Languate (TAL)',
