96702d58f4094dd10e7b50196931297e5b986643
Tres Seaver <tseaver@palladion.com>
2010-05-05 19:30:50 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -47,7 +47,7 @@ def alltests():
 
 setup(name='zope.tal',
       version = '3.5.3dev',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope 3 Template Application Languate (TAL)',
       long_description=(
