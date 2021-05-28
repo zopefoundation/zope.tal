dcb9d212acb519dda5a76f8e355e288b1b7e4b3b
Michael Howitz <mh@gocept.com>
2021-05-28 16:42:15 +0200

    Back to development: 4.6

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.tal',
-      version='4.5',
+      version='4.6.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Template Application Language (TAL)',
