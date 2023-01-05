4e666346d687d527999585b459c2cbd1ed430878
Michael Howitz <mh@gocept.com>
2023-01-05 09:49:01 +0100

    Bumped version for breaking release.

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
  Changes
 =========
 
-4.6 (unreleased)
+5.0 (unreleased)
 ================
 
 - Add support for Python 3.10.
--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.tal',
-      version='4.6.dev0',
+      version='5.0.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Template Application Language (TAL)',
